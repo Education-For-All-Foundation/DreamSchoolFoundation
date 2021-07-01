2b6abf06d8c35f955971d911e748c7417401587e
SanduniDisanayakaCS <sandunidisanayaka96@gmail.com>
2021-07-01 20:52:31 +0530

    comment section - add edit function

--- editcomment.php
+++ editcomment.php
@@ -1,3 +1,9 @@
+<?php
+	date_default_timezone_set('Asia/Colombo');
+	include 'func.php';
+	include 'dbCon.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +13,18 @@
 </head>
 <body>
 	<?php 
+		$cid = $_POST['cid'];
+		$uid = $_POST['uid'];
+		$date = $_POST['date'];
+		$message = $_POST['message'];
 
+		echo "<div class='container'><div class='row'><form method='POST' action='".editcomments($conn)."'>
+	<input type='hidden' name='cid' value='".$cid."'>
+	<input type='hidden' name='uid' value='".$uid."'>
+	<input type='hidden' name='date' value='".$date."'>
+	<textarea name='message' required>".$message."</textarea><br>
+	<button id='post' type='submit' name='commentSubmit'>Edit</button>
+	</form></div></div>";
 
 	?>
 </body>
--- func.php
+++ func.php
@@ -29,6 +29,17 @@ function getcomments($conn){
 
 }
 
+function editcomments($conn){
+	if (isset($_POST['commentSubmit'])) {
+		$cid = $_POST['cid'];
+		$uid = $_POST['uid'];
+		$date = $_POST['date'];
+		$message = $_POST['message'];
 
+		$sql = "UPDATE comments SET message='".$message."' WHERE cid='".$cid."'";
+		$result = $conn->query($sql);
+		header("Location: Comments.php");
+	}
+}
 
 ?>
