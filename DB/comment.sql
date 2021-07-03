398df2e8864dbbcc492282dcbf06ea6d39eaf8cf
SanduniDisanayakaCS <sandunidisanayaka96@gmail.com>
2021-07-03 15:51:07 +0530

    update db - inserted userD table

--- DB/comment.sql
+++ DB/comment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2021 at 07:52 AM
+-- Generation Time: Jul 03, 2021 at 12:19 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -39,8 +39,29 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`cid`, `uid`, `date`, `message`) VALUES
-(1, 'anonymous', '2021-07-01 20:54:03', 'Great work!!!!'),
-(3, 'anonymous', '2021-07-01 20:54:17', 'keep going..');
+(9, '1', '2021-07-03 14:57:09', 'Great work!!!'),
+(12, '2', '2021-07-03 14:57:57', 'Keep it up!!!'),
+(15, '2', '2021-07-03 15:47:07', 'good job!!!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userd`
+--
+
+CREATE TABLE `userd` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(128) NOT NULL,
+  `pwd` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userd`
+--
+
+INSERT INTO `userd` (`id`, `uid`, `pwd`) VALUES
+(1, 'Admin', '123'),
+(2, 'user1', '123');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +74,12 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`cid`);
 
 --
+-- Indexes for table `userd`
+--
+ALTER TABLE `userd`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,7 +87,13 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `cid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `cid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `userd`
+--
+ALTER TABLE `userd`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
