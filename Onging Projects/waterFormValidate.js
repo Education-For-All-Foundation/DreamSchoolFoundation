6415c51908c9227ea5752d529f1c782ce6fd10df
Kushani <k>
2021-07-12 22:36:34 +0530

    responsived again Drinking water project doantion form

--- Onging Projects/waterFormValidate.js
+++ Onging Projects/waterFormValidate.js
@@ -1,16 +1,16 @@
 function validateForm1(){
-    if (document.form1.name.value.length==0)   {
+    if (document.formOne.name.value.length==0)   {
         document.getElementById("nameError").innerHTML="Please enter your name!!";
         return;
     }
 
 
-    if (document.form1.mail.value.length==0)   {
+    if (document.formOne.mail.value.length==0)   {
         document.getElementById("emailError").innerHTML="Please enter your Email!!";
         return;
 
     }
-    var mail=document.form1.mail.value;
+    var mail=document.formOne.mail.value;
     var atPoss=mail.indexOf("@");
     var dotPoss=mail.lastIndexOf(".");
     if (atPoss < 1 || dotPoss<atPoss+2 || dotPoss+2>=mail.length){
@@ -18,19 +18,19 @@ function validateForm1(){
         return;
     }
 
-    if (document.form1.phone.value.length!=10)   {
+    if (document.formOne.phone.value.length!=10)   {
         document.getElementById("phoneError").innerHTML="Please enter valid phone number!!";
         return;
 
     }
 
-    if (isNaN(document.form1.phone.value)) {
+    if (isNaN(document.formOne.phone.value)) {
         document.getElementById("phoneError").innerHTML="Please enter valid phone number!!";
         return;
 
     }
 
-    if (document.form1.Address.value.length==0) {
+    if (document.formOne.Address.value.length==0) {
         document.getElementById("addressError").innerHTML="Please enter your Address!!";
         return;
 
@@ -42,20 +42,20 @@ function validateForm1(){
 
 
 function validateAmount() {
-    if (document.form2.amount.value.length == 0) {
+    if (document.formTwo.amount.value.length === 0) {
         document.getElementById("amountError").innerHTML="Please Select an Amount";
-    }else{
-    document.getElementById("successTwo").innerHTML="Your amount is selected!!!";}
-
+    }else {
+        document.getElementById("successTwo").innerHTML = "Your amount is selected!!!";
+    }
 
 }
 
 
 
 function validatePayment(){
-    if ((!document.form3.method[0].checked)&& (!document.form3.method[1].checked)&&(!document.form3.method[2].checked)){
+    if ((!document.formThree.method[0].checked)&& (!document.formThree.method[1].checked)&&(!document.formThree.method[2].checked)){
         document.getElementById("paymentError").innerHTML="Please select Your payement method";
     }else{
 
-    document.getElementById("successThree").innerHTML="Your payment method is recorded successfully!!!";}
+        document.getElementById("successThree").innerHTML="Your payment method is recorded successfully!!!";}
 }
