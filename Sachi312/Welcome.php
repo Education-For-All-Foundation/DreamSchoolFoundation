12243c7efd487d2cfc67d4cd875a6219ad6ae1a6
sachininimesha312 <sahanyapieris@gmail.com>
2021-07-15 01:08:22 +0530

    welcome

---
+++ Sachi312/Welcome.php
@@ -0,0 +1,38 @@
+
+
+<?php 
+
+include_once'connection.php';
+
+$Name=$_REQUEST['name'];
+$Password=$_REQUEST['password'];
+$Email=$_REQUEST['email'];
+echo "we are inside the php code";
+if (empty($Name)) {
+	$name_error='Please insert your User name';
+}
+if (empty($Email)) {
+	$name_error='Please insert your Email address';
+}
+if (empty($Password)) {
+	$mail_error='Please insert your Password';
+}
+
+	if(empty($Name) OR empty($Email) OR empty($Password)){
+}else{
+
+	$query="INSERT INTO user_table(user_name,email,password)VALUES('$Name','$Email','$Password');";
+	$insert=mysqli_query($connect,$query);
+
+	if ($insert) {
+			echo "data enterd succesully";
+	}else{
+	
+		echo "data not entered".mysqli_error($connect);
+
+
+	}
+}
+
+ ?>
+
